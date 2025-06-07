ffecc7329e01ab986f42ec071627ed15702a6677
KacperZurawik <kaacper181@gmail.com>
2025-06-07 03:27:18 +0200

    feat: added basic menu

--- CoreLibraryManager/Program.cs
+++ CoreLibraryManager/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool isAppRunning = true;
+
+            while (isAppRunning)
+            {
+                Console.WriteLine("1. Add a new book");
+                Console.WriteLine("2. Exit");
+
+                string? menuOption = Console.ReadLine();
+
+                switch (menuOption)
+                {
+                    case "1":
+                        break;
+
+                    case "2":
+                        Console.Clear();
+                        Console.WriteLine("See you soon!");
+                        isAppRunning = false;
+                        break;
+                }
+            }
         }
     }
 }
