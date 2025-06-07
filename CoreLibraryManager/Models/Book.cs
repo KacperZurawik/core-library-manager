35d7218f781444e9674d9c1ea18edaf0b87914c6
KacperZurawik <kaacper181@gmail.com>
2025-06-07 04:15:27 +0200

    add: basic properties for book model

--- CoreLibraryManager/Models/Book.cs
+++ CoreLibraryManager/Models/Book.cs
@@ -8,5 +8,12 @@ namespace CoreLibraryManager.Models
 {
     internal class Book
     {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Author { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public int YearPublished { get; set; }
+        public string? Genre { get; set; }
     }
 }
