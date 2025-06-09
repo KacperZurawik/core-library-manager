dbce775bf0fceb66c3def0d4aa2e706bc5fbca09
KacperZurawik <kaacper181@gmail.com>
2025-06-09 21:05:05 +0200

    refactor: remove property from Book model

--- CoreLibraryManager/Models/Book.cs
+++ CoreLibraryManager/Models/Book.cs
@@ -9,7 +9,6 @@ namespace CoreLibraryManager.Models
     internal class Book
     {
         public int Id { get; set; }
-        public string? Name { get; set; }
         public string? Author { get; set; }
         public string? Title { get; set; }
         public string? Description { get; set; }
