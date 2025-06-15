0e1bc5af051c4f30908b1a23b54f7d4819d91d16
KacperZurawik <kaacper181@gmail.com>
2025-06-15 08:39:26 +0200

    chore: change class name

---
+++ CoreLibraryManager/Services/BookService.cs
@@ -0,0 +1,18 @@
+﻿using CoreLibraryManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLibraryManager.Services
+{
+    public class BookService
+    {
+        public void AddBook()
+        {
+       
+        }
+    }
+}
